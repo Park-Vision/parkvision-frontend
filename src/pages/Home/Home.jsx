e2e437d430db57425e7c3aeadb83ef053cb49cfd
weronika <weronika.lit0@gmail.com>
2023-10-26 18:08:45 +0200

    fix layout and add custom markers

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,5 @@
-// generate a home page thah will be under header component in app.js. Set a search field with button and list of movies
 import React, {useEffect, useState} from 'react';
 
-// use @mui/material
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
@@ -10,27 +8,23 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import PlaceIcon from '@mui/icons-material/Place';
-import ListIcon from '@material-ui/icons/List';
+import ListIcon from '@mui/icons-material/List';
+import LocalParkingIcon from '@mui/icons-material/LocalParking';
 import L from 'leaflet';
-
-
-// use @mui/icons-material
 import SearchIcon from '@mui/icons-material/Search';
 import {useDispatch, useSelector} from "react-redux";
-import {getCars} from "../../actions/carActions";
 import {getParkings} from "../../actions/parkingActions";
 import { useNavigate } from "react-router-dom";
-
-import {MapContainer, Marker, Popup, TileLayer, ZoomControl} from 'react-leaflet';
+import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet';
+import {renderToString} from "react-dom/server";
 
 export default function Home() {
     const parkings = useSelector(state => state.parkingReducer.parkings)
     useSelector(state => state.parkingReducer.parking = {})
     const dispatch = useDispatch()
     const authenticationReducer = useSelector((state) => state.authenticationReducer);
-    const [showMap, setShowMap] = useState(true);
+    const [showMap, setShowMap] = useState(false);
 
     useEffect(() => {
         dispatch(getParkings())
@@ -68,12 +62,11 @@ export default function Home() {
         setShowMap(false);
     };
 
-    const greenIcon = new L.Icon({
-        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
+    const parkingIcon = new L.DivIcon({
+        html: renderToString(<LocalParkingIcon style={{ color: 'green', background: 'white', borderRadius: '4px',
+            border: '2px solid black', fontSize: '27px' }} />),
+        iconSize: [32, 32],
+        className: 'custom-icon-class',
     });
     
     return (
@@ -101,7 +94,6 @@ export default function Home() {
                     </Button>
                 </Grid>
             </div>
-
             {showMap ? (
                 <div>
                     <Grid xs={12} sm={12} md={12}>
@@ -128,7 +120,7 @@ export default function Home() {
                                         <Marker
                                             key={index}
                                             position={[parking.latitude, parking.longitude]}
-                                            icon={greenIcon}
+                                            icon={parkingIcon}
                                         >
                                             <Popup>
                                                 <div style={{ minWidth: '200px', maxWidth: '250px', padding: '10px', textAlign: 'left' }}>
@@ -158,10 +150,8 @@ export default function Home() {
                                             </Popup>
                                         </Marker>
                                     ))}
-
                                 </MapContainer>
                             </div>
-
                         </Card>
                     </Grid>
                 </div>
@@ -196,54 +186,53 @@ export default function Home() {
                             </Grid>
                         </Grid>
                     </form>
-
-                    <Grid container spacing={4} sx={{ mt: 4 }} >
+                    <Grid container spacing={4} sx={{ mt: 4 }}>
                         {parkings.map((parking, index) => (
                             <Grid item key={index} xs={12} sm={12} md={12}>
                                 <Card
                                     sx={{
                                         height: '100%',
                                         display: 'flex',
-                                        flexDirection: 'column',
+                                        flexDirection: 'row', // Ensure the content and map are displayed side by side
                                     }}
                                     onClick={() => handleClick(parking.id)}
                                 >
-                                    <div style={{ height: '400px'}}>
-                                        {parking.id && (
-                                            <MapContainer
-                                                style={{ width: '100%', height: '100%' }}
-                                                center={[parking.latitude, parking.longitude]}
-                                                zoom={18}
-                                                scrollWheelZoom={false}
-                                                zoomControl={false}
-                                                dragging={false}
-                                            >
-
-                                                <TileLayer
-                                                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                                                    url="http://mt0.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
-                                                />
-
-                                            </MapContainer>
-                                        )}
-                                    </div>
-                                    <CardContent sx={{ flexGrow: 1 }}>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            {parking.name}
-                                        </Typography>
-                                        <Typography>Address:{parking.street},{parking.zipCode} {parking.city}</Typography>
-                                        <Typography>Open hours: {parking.openHours}</Typography>
-                                        <Typography>$/h: {parking.costRate}</Typography>
-                                    </CardContent>
+                                    <Grid item xs={6} sm={6} md={6} style={{ padding: '20px', paddingRight: 0 }}>
+                                        <CardContent sx={{ flexGrow: 1 }}>
+                                            <Typography gutterBottom variant="h5" component="h2">
+                                                {parking.name}
+                                            </Typography>
+                                            <Typography>Address: {parking.street}, {parking.zipCode} {parking.city}</Typography>
+                                            <Typography>Open hours: {parking.openHours}</Typography>
+                                            <Typography>$/h: {parking.costRate}</Typography>
+                                        </CardContent>
+                                    </Grid>
+                                    <Grid item xs={6} sm={6} md={6} style={{ margin: 10, padding: 10}}>
+                                        <div style={{ height: '300px', margin: 0, padding: 0 }}>
+                                            {parking.id && (
+                                                <MapContainer
+                                                    style={{ width: '100%', height: '100%', borderRadius: '4px', margin: 0, padding: 0 }}
+                                                    center={[parking.latitude, parking.longitude]}
+                                                    zoom={18}
+                                                    scrollWheelZoom={false}
+                                                    zoomControl={false}
+                                                    dragging={false}
+                                                >
+                                                    <TileLayer
+                                                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                                        url="http://mt0.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+                                                    />
+                                                </MapContainer>
+                                            )}
+                                        </div>
+                                    </Grid>
                                 </Card>
                             </Grid>
                         ))}
                     </Grid>
                 </div>
             )}
-
-
         </Box>
         </Container>
     );
-    }
+}
