e3c5975ccd1fe9971b1cc2da727708185c75ef06
Filip Strózik <47420153+Filipstrozik@users.noreply.github.com>
2023-11-05 16:57:29 +0100

    KAN-78 addiing and editing spots (#20)
    * mission subpage
    
    * wip
    
    * fix requesting cars for manager
    
    * request to save parking
    
    * wip working adding bulk new spots
    
    * wip only parking manager acces to editor
    
    * wip edit one spot
    
    * KAN-78  adding and editing spots wokring
    
    * fix merge of 20 conflicted files
    
    * KAN-78 frontend fix
    
    ---------
    
    Co-authored-by: Szymon Romanek <srszymonsr@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import AppBar from "./components/AppBar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Reservations from "./pages/Reservations/Reservations";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
@@ -15,7 +15,11 @@ import ReservationDetails from "./pages/Reservations/ReservationDetails";
 import ManagerProfile from "./pages/ParkingManagement/ManagerProfile";
 import UserProfile from "./pages/User/UserProfile";
 import * as React from "react";
+import ParkingSpotDetails from "./pages/ParkingSpot/ParkingSpotDetails";
+import ParkingEditor from "./pages/Editor/Editor"
+
 const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
 function App() {
     const [mode, setMode] = React.useState("light");
     const colorMode = React.useMemo(
@@ -57,51 +61,20 @@ function App() {
                     <Toolbar />
                     <div className='home'>
                         <Routes>
-                            <Route
-                                exact
-                                path={"/"}
-                                element={<Home />}
-                            />
-                            <Route
-                                path={"/login"}
-                                element={<Login />}
-                            />
-                            <Route
-                                path={"/register"}
-                                element={<Register />}
-                            />
-                            <Route
-                                path={"/reservations"}
-                                element={<Reservations />}
-                            />
-                            <Route
-                                path={"/about"}
-                                element={<About />}
-                            />
-                            <Route
-                                path={"/contact"}
-                                element={<Contact />}
-                            />
-                            <Route
-                                path={"/cars"}
-                                element={<CarPage />}
-                            />
-                            <Route
-                                path={"/parking/:parkingId"}
-                                element={<ParkingDetails />}
-                            />
-                            <Route
-                                path={"/reservation-details"}
-                                element={<ReservationDetails />}
-                            />
-                            <Route
-                                path={"/management"}
-                                element={<ManagerProfile />}
-                            />
-                            <Route
-                                path={"/profile"}
-                                element={<UserProfile />}
-                            />
+                            <Route exact path={'/'} element={<Home/>}/>
+                            <Route path={'/login'} element={<Login/>}/>
+                            <Route path={'/register'} element={<Register/>}/>
+                            <Route path={'/reservations'} element={<Reservations/>}/>
+                            <Route path={'/about'} element={<About/>}/>
+                            <Route path={'/contact'} element={<Contact/>}/>
+                            <Route path={'/cars'} element={<CarPage/>}/>
+                            <Route path={'/parking/:parkingId/editor'} element={<ParkingEditor/>}/>
+                            <Route path={'/parking/:parkingId'} element={<ParkingDetails />} />
+                            <Route path={"/parkingspot/:parkingSpotId"} element={<ParkingSpotDetails />}/>
+                            <Route path={"/reservation-details"} element={<ReservationDetails />}/>
+                            <Route path={"/management"} element={<ManagerProfile />}/>
+                            <Route path={"/profile"} element={<UserProfile />}/>
+
                         </Routes>
                     </div>
                 </ThemeProvider>
