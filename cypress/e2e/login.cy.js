05643194bdd488fbeed90f6244ccfcc4e55c3bc6
Filipstrozik <filipshelby@gmail.com>
2023-11-19 14:14:54 +0100

    e2e login and register

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,18 @@
 describe('template spec', () => {
   it('renders login page', () => {
     cy.visit('http://localhost:3000/login')
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('.MuiCardMedia-root').should('be.visible');
+    cy.get('[data-cy="login-title"]').should('be.visible');
+    cy.get('[data-cy="login-title"]').should('have.text', 'Login');
+    cy.get('#\\:r0\\:').should('be.visible');
+    cy.get('#\\:r1\\:').should('be.enabled');
+    cy.get('[data-cy="password-reset-button"]').should('be.visible');
+    cy.get('[data-cy="password-reset-button"]').should('be.enabled');
+    cy.get('[data-cy="login-button"]').should('be.disabled');
+    cy.get('[data-cy="register-button"]').should('be.visible');
+    cy.get('[data-cy="register-button"]').should('be.enabled');
+    /* ==== End Cypress Studio ==== */
   })
 
   /* ==== Test Created with Cypress Studio ==== */
@@ -25,44 +37,19 @@ describe('template spec', () => {
     /* ==== End Cypress Studio ==== */
   });
 
-  /* ==== Test Created with Cypress Studio ==== */
-  it('login invalid user', function() {
-    /* ==== Generated with Cypress Studio ==== */
+  it('login invalid user', function () {
     cy.visit('http://localhost:3000/login');
-    cy.get('#\\:r0\\:').clear('u');
-    cy.get('#\\:r0\\:').type('invalidmail');
-    cy.get('#\\:r1\\:').clear();
-    cy.get('#\\:r1\\:').type('invalidpassword');
-    /* ==== End Cypress Studio ==== */
+
     /* ==== Generated with Cypress Studio ==== */
-    cy.get('[type="submit"]').click();
+    cy.get('#\\:r0\\:').clear('test@pv.pl');
+    cy.get('#\\:r0\\:').type('test@pv.pl');
+    cy.get('#\\:r1\\:').clear('F');
+    cy.get('#\\:r1\\:').type('Filip123!');
+    cy.get('[data-cy="login-button"]').should('be.enabled');
+    cy.get('[data-cy="login-button"]').click();
     cy.get('.Toastify__toast-body > :nth-child(2)').should('be.visible');
     cy.get('.Toastify__toast-body > :nth-child(2)').should('have.text', 'Please enter valid email and password');
     /* ==== End Cypress Studio ==== */
   });
 
-  /* ==== Test Created with Cypress Studio ==== */
-  it('login invalid password user', function() {
-    /* ==== Generated with Cypress Studio ==== */
-    cy.visit('http://localhost:3000/login');
-    cy.get('#\\:r0\\:').clear('filipshelby@gmail.com');
-    cy.get('#\\:r0\\:').type('filipshelby@gmail.com');
-    cy.get('#\\:r1\\:').clear('F');
-    cy.get('#\\:r1\\:').type('Filip123!!');
-    cy.get('[type="submit"]').click();
-    cy.get('.Toastify__toast-icon > svg').should('be.visible');
-    /* ==== End Cypress Studio ==== */
-    /* ==== Generated with Cypress Studio ==== */
-    cy.get('.Toastify__toast-body > :nth-child(2)').should('have.text', 'Please enter valid email and password');
-    /* ==== End Cypress Studio ==== */
-  });
-
-  /* ==== Test Created with Cypress Studio ==== */
-  it('login no pass user', function() {
-    /* ==== Generated with Cypress Studio ==== */
-    cy.visit('http://localhost:3000/login');
-    cy.get('[type="submit"]').click();
-    cy.get('#\\:r0\\:').should('be.enabled');
-    /* ==== End Cypress Studio ==== */
-  });
 })
