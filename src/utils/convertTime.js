adf259e9e53f9dbeae02363b6c52a11c6a3e9bad
Filipstrozik <filipshelby@gmail.com>
2023-11-19 18:18:40 +0100

    Refactor convertTime function to remove unnecessary code

--- src/utils/convertTime.js
+++ src/utils/convertTime.js
@@ -12,7 +12,6 @@ const convertTime = (time, timezoneOffset) => {
     }
     // Create a new Date object with the time
     var date = new Date(`1970-01-01T${time}`);
-    // Parse the desired timezone offset
     var desiredOffset = parseInt(timezoneOffset);
 
     // if desiredOffset is NaN then return time as is
@@ -21,7 +20,6 @@ const convertTime = (time, timezoneOffset) => {
     }
     // Add the desired offset to the date
     date.setUTCHours(date.getUTCHours() + desiredOffset);
-    // Format the date to get the time in HH:MM format
     var adjustedTime = date.toISOString().substring(11, 16);
     return adjustedTime;
 }
