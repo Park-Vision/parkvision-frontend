e09ff6e6ea1fe450709c0fcd484c9bea83440725
Filipstrozik <filipshelby@gmail.com>
2023-11-23 22:15:14 +0100

    KAN-110 fix after cr

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,10 +7,10 @@ export default function About() {
                 <h1>ABOUT</h1>
                 <p>ParkVision is a parking reservation system</p>
                 <p>
-                    ParkVision allows users to easily find and reserve parking spaces in various locations.
+                    ParkVision allows users to easily find and reserve parking spots in various locations.
                     With our user-friendly interface, you can search for available parking spots, view pricing
                     information, and make reservations with just a few clicks. Say goodbye to the hassle of
-                    searching for parking spaces and enjoy a seamless parking experience with ParkVision.
+                    searching for parkings and enjoy a seamless parking experience with ParkVision.
                 </p>
             </Box>
         </Container>
--- src/pages/Login/Register.jsx
+++ src/pages/Login/Register.jsx
@@ -73,7 +73,8 @@ export default function Register() {
         } else if (!validateName(lastName)) {
             toast.info('Wrong last name');
         } else if (!validatePassword(password)) {
-            toast.info('Password must contains eight characters, including at least one capital letter and number');
+            toast.info('Password must contains eight characters or more, including at least one capital ' +
+                'letter, special character and a number.');
         } else if (passwordRepeat !== password) {
             toast.info('Passwords must be the same');
         } else {
