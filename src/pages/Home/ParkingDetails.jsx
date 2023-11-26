be301943fb5db17da54b0db097b20ee74bb5d4e5
Filipstrozik <filipshelby@gmail.com>
2023-11-26 20:21:15 +0100

    minal approach for 24h support

--- src/pages/Home/ParkingDetails.jsx
+++ src/pages/Home/ParkingDetails.jsx
@@ -141,6 +141,9 @@ function ParkingDetails(props) {
             return true;
         }
 
+        if (isParking24h(parking)) {
+            return false;
+        }
 
         let hour = value.hour();
         let minute = value.minute();
@@ -175,7 +178,7 @@ function ParkingDetails(props) {
         if (!parking) {
             return false;
         }
-        return convertTime(parking.startTime, parking.timeZone) === "00:00" && convertTime(parking.endTime, parking.timeZone) === "23:45";
+        return convertTime(parking.startTime, parking.timeZone) === "00:00" && convertTime(parking.endTime, parking.timeZone) === "00:00";
     };
 
 
@@ -207,6 +210,7 @@ function ParkingDetails(props) {
 
         if (startTime.toDate().getTime() >= endTime.toDate().getTime()) {
             endTime = startTime.add(15, "minute");
+            endDay = endTime;
         }
         setEndTime(endTime);
 
@@ -357,8 +361,6 @@ function ParkingDetails(props) {
         });
 
         navigate("/reservation-details");
-
-        // dispatch(addReservation(newReservation))
     };
 
     const handleGoToEditor = () => {
@@ -440,13 +442,19 @@ function ParkingDetails(props) {
                                                     interactive>
                                                     {occupiedParkingSpotsMap && occupiedParkingSpotsMap[parkingSpot.id] && (
                                                         <Popup>
-                                                            {`This spot will be available from: ${convertDate(occupiedParkingSpotsMap[parkingSpot.id].earliestStart)}`} <br></br>
-                                                            {`This spot will be available to: ${convertDate(occupiedParkingSpotsMap[parkingSpot.id].earliestEnd)}`}
+                                                            Available from: <br />
+                                                            {`${convertDate(occupiedParkingSpotsMap[parkingSpot.id].earliestStart)}`}
+                                                            {occupiedParkingSpotsMap[parkingSpot.id].earliestEnd && (
+                                                                <div>
+                                                                    Available to: <br />
+                                                                    {convertDate(occupiedParkingSpotsMap[parkingSpot.id].earliestEnd)}
+                                                                </div>
+                                                            )}
                                                         </Popup>
                                                     )}
                                                     {occupiedParkingSpotsMap && occupiedParkingSpotsMap[parkingSpot.id] == null && (
                                                         <Popup>
-                                                            {`This spot will not be available for the rest of the day.`}
+                                                            {`Not be available for the rest of the day.`}
                                                         </Popup>
                                                     )}
                                                 </Polygon>
@@ -724,7 +732,7 @@ function ParkingDetails(props) {
                     </Grid>
                 </Grid>
             </Box>
-        </Container>
+        </Container >
     );
 }
 
--- src/pages/ParkingManagement/ManagerParkingDetails.jsx
+++ src/pages/ParkingManagement/ManagerParkingDetails.jsx
@@ -271,8 +271,6 @@ function ManagerParkingDetails() {
             });
         }
 
-
-
     }, [parkingId]);
 
 
